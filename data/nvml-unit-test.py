23c05806ea41f0edcaaace9c32d539db9847fb6d
JinwangMok <jinwangmok@gmail.com>
2024-02-13 17:00:56 +0900

    test: dynamic model selection with nvml

--- data/nvml-unit-test.py
+++ data/nvml-unit-test.py
@@ -19,12 +19,12 @@ with open(model_size_fp, "r") as file:
     model_size_list = yaml.safe_load(file)['models']
 
 # (2) Get current GPU available memory.
-free_gpu_mem = None
+available_gpu_mem = None
 occupied_mem_per_proc = {}
 device_count = pynvml.nvmlDeviceGetCount()
 for i in range(device_count):
     handle = pynvml.nvmlDeviceGetHandleByIndex(i)
-    free_gpu_mem = pynvml.nvmlDeviceGetMemoryInfo(handle).free # Byte
+    available_gpu_mem = pynvml.nvmlDeviceGetMemoryInfo(handle).free # Byte
     try:
         procs = pynvml.nvmlDeviceGetComputeRunningProcesses(handle)
         for proc in procs:
@@ -33,5 +33,25 @@ for i in range(device_count):
         print(f"  Error retrieving processes: {e}")
 
 # (3) Compare (1) and (2)
-print(free_gpu_mem)
-print(occupied_mem_per_proc)
+model_candidates = []
+for model_data in model_size_list:
+    if model_data["size"] < available_gpu_mem:
+        print(f"We can use {model_data['name']}.")
+        model_candidates.append(model_data)
+
+# (4) Select LLM backend.
+selected_model = None
+if len(model_candidates) < 1:
+    print("We have to use MobileX backend. Local GPU is unavailable now.")
+else:
+    selected_model_size = 0
+    for model_data in model_candidates:
+        if model_data["size"] > selected_model_size:
+            selected_model = model_data["name"]
+            selected_model_size = model_data["size"]
+
+# (5) Final Decision
+if selected_model:
+    print(selected_model)
+else:
+    print("MobileX Backend")
